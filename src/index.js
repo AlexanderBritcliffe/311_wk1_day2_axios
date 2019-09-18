c967b92a450b1324aeedae9bfc41516e45c6abc8
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2019-09-17 19:33:13 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
-// import axios here
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+const axios = require('axios');
+const api_key = 'd771b19ef336ed8381def3a60b574464';
 
 const discoverMovie = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
-  // code here
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`;
+  return axios.get(url)
+  .then(response => {
+    console.log(response);
+  return result.data.status;
+  })
+  .catch(error => {
+    console.log(error);
+  });
 }
 
 const getMovieById = (id) => {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`;
+  return axios.get("/user?ID=34611")
+  .then(response => {
+    console.log(response);
+  return result.data.Original_title;
+  })
+  .catch(error => {
+    console.log(error);
+  });
 }
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
-  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
-  // code here
+  const fakeId = 5783; // FAKE ID HERE
+  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`;
+  return axios.get("/user?ID=5783")
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error.response.status);
+  });
 }
 
 
--- test/index.test.js
+++ test/index.test.js
@@ -2,29 +2,37 @@ const {
   discoverMovie,
   getMovieById,
   getMovieByIdFailure
-} = require('../src')
+  } = require('../src')
+  
+  describe('API protocol practice', () => {
+  // Note that is is not best practice to make actual API calls with 
+  // these tests. Normally, you would stub these calls or use nock 
+  // to prevent an actual network call and mock the responses for 
+  // unit testing. This structure is in place for practice only. 
+  
 
-describe('API protocol practice', () => {
-// Note that is is not best practice to make actual API calls with 
-// these tests. Normally, you would stub these calls or use nock 
-// to prevent an actual network call and mock the responses for 
-// unit testing. This structure is in place for practice only. 
-
-  test('It should make a request to the discover endpoint', async () => {
-    const result = await discoverMovie()
-    expect(result).toHaveProperty('data.results')
-    expect(result).toHaveProperty('status')
-    expect(result).toHaveProperty('headers')
-  })
-
-  test('It should get a movie by it\'s id', async () => {
-    const data = await getMovieById(500)
-    expect(data.original_title).toBe('Reservoir Dogs')
-  })
-
-  test('It should FAIL to get a movie by it\'s id and return a 404', async () => {
-    const status = await getMovieByIdFailure()
-    expect(status).toEqual(404)
-  })
-
-})
+  test('It should make a request to the discover endpoint',  () => {
+    const result = discoverMovie()
+    result.then((d)=>{
+    expect(d).toHaveProperty('data.results')
+    expect(d).toHaveProperty('status')
+    expect(d).toHaveProperty('headers')
+    })
+    
+    })
+    
+    test('It should get a movie by it\'s id',  () => {
+    const data = getMovieById(500).then((d)=>{
+    expect(d.original_title).toBe('Reservoir Dogs')
+    });
+    
+    })
+    
+    test('It should FAIL to get a movie by it\'s id and return a 404',  () => {
+    const status = getMovieByIdFailure().then((s)=>{
+    expect(s).toEqual(404)
+    });
+    
+    })
+    
+    })
